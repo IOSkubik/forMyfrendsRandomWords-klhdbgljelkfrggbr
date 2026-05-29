934253fce6daf3604647d6bd2c465ec9e46b649a
IOSkubik <umb.agent.rella@gmail.com>
2026-05-30 00:18:49 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,10 +1,10 @@
 import base64
 
-with open("configsVlessRealty.txt", "r", encoding="utf-8") as f:
+with open("vpn_configs.txt", "r", encoding="utf-8") as f:
     content: str = f.read().strip()
     encoded: str = base64.b64encode(content.encode("utf-8")).decode("utf-8")
 
-with open("JerkStyle.txt", "w", encoding="utf-8") as f:
+with open("vpnall.txt", "w", encoding="utf-8") as f:
     f.write(encoded)
 
 print("OK")
