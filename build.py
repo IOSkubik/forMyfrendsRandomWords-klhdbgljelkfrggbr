5259cf710f75d487a937c0c817f1551b1c652c7f
IOSkubik <umb.agent.rella@gmail.com>
2026-05-29 23:33:31 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,13 +1,13 @@
 import base64
 
 with open("configsVlessRealty.txt", "r", encoding="utf-8") as f:
-content = f.read().strip()
+    content = f.read().strip()
 
 encoded = base64.b64encode(
-content.encode("utf-8")
+    content.encode("utf-8")
 ).decode("utf-8")
 
 with open("JerkStyle.txt", "w", encoding="utf-8") as f:
-f.write(encoded)
+    f.write(encoded)
 
 print("JerkStyle.txt updated")
