e08883020296e6656d9ce23ca5163caa2a0b1a24
IOSkubik <umb.agent.rella@gmail.com>
2026-05-29 22:57:14 +0300

    Update build.py

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ encoded = base64.b64encode(
 content.encode("utf-8")
 ).decode("utf-8")
 
-with open("subscription.txt", "w", encoding="utf-8") as f:
+with open("JerkStyle.txt", "w", encoding="utf-8") as f:
 f.write(encoded)
 
 print("JerkStyle.txt updated")
